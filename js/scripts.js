3717ed87f6b989d10f3ebe2d6c5b2bec034f9c67
“SivonneWard” <sivonne@live.unc.edu>
2018-11-14 12:38:11 -0500

    git commit, set up the files

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  console.log('scripts loaded');
+  var myKey = config.MY_KEY;
+  var url = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=7314196a8ad34c71ae3fdae73371b511' + myKey;
+
+});
